bd9746ea8f2c69fab0cdc875a4cdf3ff3ef7bb2a
Boris <borisberestov@ya.ru>
2023-06-01 16:22:35 +0300

    Базовая настройка и окружение

--- project/settings.py
+++ project/settings.py
@@ -11,21 +11,31 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import multiprocessing
+
+env = environ.Env(
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, '1'),
+    REDIS_PASSWORD=(str, 'root')
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6on4%ws68ncgx2x(c&b1*m)n2!-pe^cq3-!z9=df$_vy%)6$55'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
+    'django_celery_results',
+    'parser',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -116,8 +129,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
+
+CELERY_TIMEZONE = 'Europe/Moscow'
+CELERY_TASK_TRACK_STARTED = True
+CELERY_IGNORE_RESULT = True
+CELERY_TASK_TIME_LIMIT = 60 * 60 * 4
+CELERY_BROKER_URL = f'redis://:{env("REDIS_PASSWORD")}@localhost:6379/0'
+CELERY_RESULT_BACKEND = f'redis://:{env("REDIS_PASSWORD")}@localhost:6379/1'
+CELERY_WORKER_CONCURRENCY = (multiprocessing.cpu_count() * 2 + 1)
+CELERYD_MAX_TASKS_PER_CHILD = 10
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
