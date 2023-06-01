f962f1f2731452358154721dd2d2b2f717cfa70e
Boris <borisberestov@ya.ru>
2023-06-01 17:25:16 +0300

    Админка

--- parser/admin.py
+++ parser/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from parser import models
 
-# Register your models here.
+
+@admin.action(description="Пометить как отправленные")
+def make_sent(model_admin, request, queryset):
+    queryset.update(sent=True)
+
+
+@admin.action(description="Пометить как не отправленные")
+def make_unsent(model_admin, request, queryset):
+    queryset.update(sent=False)
+
+
+@admin.action(description="Пометить как активные")
+def make_active(model_admin, request, queryset):
+    queryset.update(is_active=True)
+
+
+@admin.action(description="Пометить как не активные")
+def make_inactive(model_admin, request, queryset):
+    queryset.update(is_active=False)
+
+
+class SegmentInline(admin.StackedInline):
+    model = models.Segment
+    extra = 1
+
+
+class PairSegmentInline(admin.StackedInline):
+    model = models.PairSegment
+    extra = 0
+
+
+@admin.register(models.Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'is_active')
+    list_filter = ('is_active',)
+    inlines = [SegmentInline]
+    actions = (make_active, make_inactive)
+
+
+@admin.register(models.Pair)
+class PairAdmin(admin.ModelAdmin):
+    list_display = ('token', 'site', 'sent')
+    readonly_fields = ('site', 'token', 'sent')
+    list_filter = ('site', 'sent')
+    search_fields = ('pair_name',)
+    actions = (make_sent, make_unsent)
+    inlines = [PairSegmentInline]
+
+
+@admin.register(models.Bot)
+class ParseTimeAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
--- parser/models.py
+++ parser/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    name = models.CharField('Название сайта', max_length=150)
+    is_active = models.BooleanField('Активен', default=True)
+    list_api_link = models.URLField('Ссылка на список')
+
+    class Meta:
+        verbose_name = 'Сайт'
+        verbose_name_plural = 'Сайты'
+
+    def __str__(self):
+        return self.name
+
+
+class Segment(models.Model):
+    json_name = models.CharField('Название json поля', max_length=20)
+    api_link = models.URLField('Ссылка на сегмент')
+    is_active = models.BooleanField('Активен', default=True)
+    site = models.ForeignKey('Site', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Сегмент'
+        verbose_name_plural = 'Сегменты'
+
+
+class Pair(models.Model):
+    site = models.ForeignKey('Site', on_delete=models.CASCADE)
+    token = models.CharField('Индивидуальный токен валюты', max_length=100)
+    sent = models.BooleanField('отправленно', default=False)
+    json_scheme = models.TextField('Схема обработки ответов')
+
+    def __str__(self):
+        return self.token
+
+    class Meta:
+        verbose_name = 'Пара'
+        verbose_name_plural = 'Пары'
+        unique_together = [('site', 'token')]
+
+
+class PairSegment(models.Model):
+    json_name = models.CharField('Название json поля', max_length=20)
+    content = models.TextField('json контент')
+    pair = models.ForeignKey('Pair', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Сегмент пары'
+        verbose_name_plural = 'Сегменты пары'
+        unique_together = [('json_name', 'pair')]
+
+
+class Bot(models.Model):
+    chat_id = models.CharField('Id чата', max_length=50)
+    bot_token = models.CharField('Токен Бота', max_length=150)
+
+    def __str__(self):
+        return self.chat_id
+
+    class Meta:
+        verbose_name = 'Бот'
+        verbose_name_plural = 'Боты'
