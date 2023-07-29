5ef15cddff18406f0348a84fcfdf2eef6c44f12f
fox-sys <borisberestov@ya.ru>
2023-07-29 21:48:50 +0300

    поправить путь

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/', admin.site.urls)
+    path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
